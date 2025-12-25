913ea641028c53e873692250fe2b544656549cf3
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-25 15:03:19 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,27 +3,27 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="home-container">
-      <h1 className="title glow">
-        🎄 Feliz Navidad 🎅
+      <h1 className="title">
+        Feliz Navidad
       </h1>
 
-      <h2 className="subtitle fade">
+      <h2 className="subtitle">
         y un próspero 2026
       </h2>
 
-      <p className="message fade">
+      <p className="message">
         Les desea
       </p>
 
-      <p className="name shine">
+      <p className="name">
         Jorge Patricio Santamaría Cherrez
       </p>
 
-      <p className="blessing fade">
+      <p className="blessing">
         Que estas fiestas estén llenas de paz, amor y esperanza ✨
       </p>
 
-      <footer className="footer fade">
+      <footer className="footer">
         ❄️ Felices Fiestas ❄️
       </footer>
     </div>
