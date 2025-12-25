c3c108572ea63e03f7f1f404b1c88a6373252513
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-25 12:28:23 -0500

    Update SantaFly.jsx

--- src/components/SantaFly.jsx
+++ src/components/SantaFly.jsx
@@ -1,16 +1,17 @@
 import { motion } from "framer-motion";
 import { Howl } from "howler";
+import santa from "../assets/santa.png";
 
 const hoho = new Howl({
-  src: ["https://www.myinstants.com/media/sounds/ho-ho-ho.mp3"],
+  src: ["https://www.myinstants.com/media/sounds/ho-ho-ho.mp3"]
 });
 
 export default function SantaFly() {
   return (
     <motion.img
-      src="https://i.ibb.co/1K5C0yN/santa-fly.png"
+      src={santa}
       alt="Santa volando"
-      style={{ position: "fixed", top: "20%", width: "250px", zIndex: 50 }}
+      style={{ position: "fixed", top: "20%", width: "200px", zIndex: 50 }}
       animate={{ x: ["-300px", "110%"] }}
       transition={{ duration: 8, repeat: Infinity, delay: 2 }}
       onClick={() => hoho.play()}
