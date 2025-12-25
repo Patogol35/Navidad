da0ba8d3f179ea0e9430c34eb9623eedb17bc119
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-25 11:46:31 -0500

    Update MusicPlayer.jsx

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,1 +1,16 @@
+import { Howl } from "howler";
+import { useEffect } from "react";
 
+const music = new Howl({
+  src: ["https://www.bensound.com/bensound-music/bensound-christmastime.mp3"],
+  loop: true,
+  volume: 0.4
+});
+
+export default function MusicPlayer() {
+  useEffect(() => {
+    music.play();
+    return () => music.stop();
+  }, []);
+  return null;
+}
