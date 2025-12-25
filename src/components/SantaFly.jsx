f872bb9d5b2a8e882b49b6d15ac6737028177e6c
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-25 12:59:47 -0500

    Update SantaFly.jsx

--- src/components/SantaFly.jsx
+++ src/components/SantaFly.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { Howl } from "howler";
-import santa from "../assets/santa.png";
+import santa from "../assets/Santa.png";
 
 const hoho = new Howl({
   src: ["https://www.myinstants.com/media/sounds/ho-ho-ho.mp3"]
