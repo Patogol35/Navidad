6f303e039a4bb1df28614eda73fb1d57393b6222
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-25 12:01:58 -0500

    Update Lights.jsx

--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import "./lights.css";
 
+const Lights = () => {
+  const bulbs = Array.from({ length: 30 });
+
+  return (
+    <ul className="bulbs">
+      {bulbs.map((_, index) => (
+        <li className="bulb" key={index}></li>
+      ))}
+    </ul>
+  );
+};
+
+export default Lights;
