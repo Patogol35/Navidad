f2ec73dda5f36839363aa072dc6fa218fcd448d6
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-25 11:44:55 -0500

    Update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,1 +1,19 @@
+import { createContext, useState, useEffect } from "react";
 
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [dark, setDark] = useState(localStorage.getItem("dark") === "true");
+  const toggleTheme = () => setDark(!dark);
+
+  useEffect(() => {
+    localStorage.setItem("dark", dark);
+    document.body.style.background = dark ? "#111" : "#1b3f5a";
+  }, [dark]);
+
+  return (
+    <ThemeContext.Provider value={{ dark, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
