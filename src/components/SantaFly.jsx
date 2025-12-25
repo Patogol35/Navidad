61570bbe32137b49bf33e86bf31c9b2bf3d04530
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-25 12:23:31 -0500

    Update SantaFly.jsx

--- src/components/SantaFly.jsx
+++ src/components/SantaFly.jsx
@@ -8,7 +8,8 @@ const hoho = new Howl({
 export default function SantaFly() {
   return (
     <motion.img
-      src="https://i.ibb.co/gTzt3dM/santa-sleigh.png"
+      src="https://i.ibb.co/1K5C0yN/santa-fly.png"
+      alt="Santa volando"
       style={{ position: "fixed", top: "20%", width: "250px", zIndex: 50 }}
       animate={{ x: ["-300px", "110%"] }}
       transition={{ duration: 8, repeat: Infinity, delay: 2 }}
