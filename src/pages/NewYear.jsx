467d609f30f0f319687331c0c09774b085a9055f
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-25 11:47:32 -0500

    Update NewYear.jsx

--- src/pages/NewYear.jsx
+++ src/pages/NewYear.jsx
@@ -1,1 +1,10 @@
+import FireworksCanvas from "../components/FireworksCanvas";
 
+export default function NewYear() {
+  return (
+    <>
+      <h1 style={{textAlign:"center", color:"#fff", marginTop:80}}>🎆 ¡Feliz Año Nuevo!</h1>
+      <FireworksCanvas />
+    </>
+  );
+}
